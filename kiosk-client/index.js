6814c9abde38f476e78a493356ffe2e26363a797
ivanko.pavel <itadm@varius.com.ua>
2019-10-30 21:53:42 +0200

    wednesday

---
+++ kiosk-client/index.js
@@ -0,0 +1,52 @@
+const socketUrl = 'http://localhost:9000';
+
+let connectButton;
+let disconnectButton;
+let socket;
+let statusInput;
+let tokenInput;
+let dataSend;
+
+const connect = () => {
+  socket = io(socketUrl, {
+    autoConnect: false,
+  });
+
+  socket.on('connect', () => {
+    console.log('Connected');
+    statusInput.value = 'Connected';
+    connectButton.disabled = true;
+    disconnectButton.disabled = false;
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log(`Disconnected: ${reason}`);
+    statusInput.value = `Disconnected: ${reason}`;
+    connectButton.disabled = false;
+    disconnectButton.disabled = true;
+  })
+
+
+  socket.on('response', (data) => {
+    console.log(`Message from server: ${data}`);
+  })
+
+  socket.open();
+};
+
+const disconnect = () => {
+  socket.disconnect();
+}
+
+const data = () => {
+	socket.emit('message','test data');
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  connectButton = document.getElementById('connect');
+  disconnectButton = document.getElementById('disconnect');
+  statusInput = document.getElementById('status');
+  tokenInput = document.getElementById('token');
+  dataSend = document.getElementById('data');
+});
--- src/app.js
+++ src/app.js
@@ -1,127 +1,23 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const fs = require("fs");
-const moment = require('moment');
 
-const defaultClientData =
-{
-    "client": [],
-    "presentations": [],
-    "videos": []
-}
-let configuration = {
-    "client": [],
-    "presentations": [],
-    "videos": []
-};
+const http = require('http');
+const io = require('socket.io')();
 
+const PORT = process.env.PORT || 9000;
+const server = http.createServer();
 
-function log(message) {
-    fs.appendFile(__dirname + "/kiosk-server.log", "\n" + moment().format('YYYY-MM-DD:hh:mm:ss') + " " + message, () => { });
-    console.log(moment().format('YYYY-MM-DD:hh:mm:ss') + " " + message);
-}
+io.attach(server);
 
-function loadConfig(newconfiguration) {    
-    try {
-        return Object.assign({},newconfiguration);
-    }
-    catch (e) {
-        log('got error ' + e);
-        return configuration;
-    }
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected.`);
 
-}
+  socket.on('message', (data) => {
+    console.log(`Socket ${socket.id} sent ${data}`);
+	socket.emit('response','like hell');
+  });
 
-function refreshConfig(resultf) {
-
-    fs.readFile(__dirname + "/kiosk-server.json", "utf8",
-        resultf);
-
-}
-
-function saveConfig() {
-    fs.writeFile(__dirname + "/kiosk-server.json", configuration,
-        function (error) {
-            if (error) {
-                log('error writing config file' + error);
-            }
-        });
-}
-const Presentations = [];
-
-
-io.on("connection", socket => {
-    log('Here is a connection from ' + socket.handshake.address);
-    // let previousPID; //presentation id
-    // const safeJoin = currentPID => {
-    //    if (currentPID==previousPID) return; //is necessary?
-    //   socket.leave(previousPID);
-    //   socket.join(currentPID);
-    //   previousPID = currentPID;
-    //   let hasChanges=false;
-    //   for (let key in configuration){
-    //       if (key=="clientIP"){
-    //           if (configuration[key]==socket.handshake.address) 
-    //             if (configuration.selPres!=currentPID) //may  never run because there is check before
-    //             {
-    //                 hasChanges=true;
-    //                 configuration.selPres=currentPID;
-    //             }
-    //       }
-    //   }
-    //   if (hasChanges) saveConfig();
-
-    socket.on("getPresentation", PID => {
-        if ((PID < Presentations.length()) && (PID >= 0)) {
-            if (Presentations[PID]) {
-                safeJoin(PID);
-                socket.emit("PresentationUpdate", Presentations[PID]); //take presentation by ID and send to all subscribed clients
-
-            }
-        }
-    });
-
-    socket.on("getConfig", () => {
-        refreshConfig(function (error, data) {
-            if (error) {
-                log('error reading config file' + error);
-            }
-            else {
-                configuration = loadConfig(JSON.parse(data));
-                let clientconfig = {
-                    "client": [],
-                    "presentations": [],
-                    "videos": []
-                };
-                clientconfig.client.push(configuration.client.filter(function (el) { return el.ip == socket.handshake.address }));
-                configuration.presentations.forEach(function (el) { clientconfig.presentations.push(el) });
-                configuration.videos.forEach(function (el) { clientconfig.videos.push(el) });
-                socket.emit("Configuration", JSON.stringify(clientconfig));
-            }
-        });
-    });
-    socket.emit("connected");
+  socket.on('disconnect', () => {
+    console.log(`Socket ${socket.id} disconnected.`);
+  });
 });
 
-log('kiosk-server started');
-http.listen(3443, '0.0.0.0');
-app.get('/PresentationData/:name', function (req, res,next) {
-    var options = {
-        root: __dirname+'/PresentationData',
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-      }
-      res.type('png');    
-      var fileName = req.params.name
-      res.sendFile(fileName, options, function (err) {
-        if (err) {
-          next(err)
-        } 
-      })
-  
-  });
-  
+server.listen(PORT);
